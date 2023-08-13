d1b05ce9c698bdb45156d73aabf2650bfbabd340
nandhinibrj <>
2023-08-13 13:38:28 -0400

    Updated package lock json

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Typography variant="h4" padding={1}>
+        <Typography variant="h4" padding={1.5}>
           Hotel Reservation System
         </Typography>
       </header>
